8fce491f2456cc3534a01c32b777385f36ad12e8
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2021-02-07 03:05:16 -0700

    Make submit functionality work

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import TodoList from './components/TodoList';
 
 function App() {
   const[inputText, setInputText] = useState("");
+  const[todos, setTodos] = useState([]);
 
   return (
     <div className="App">
       <h1>This is a test: {inputText}</h1>
 
-      <Form setInputText={setInputText} />
-      <TodoList />
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList todos={todos} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit } from '@fortawesome/free-solid-svg-icons'
 
-const Form = ({ setInputText }) => {
+const Form = ({ setInputText, todos, setTodos, inputText }) => {
 
 
     const inputTextHandler = (e) => {
@@ -12,12 +12,30 @@ const Form = ({ setInputText }) => {
     };
 
 
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+
+        // Add a new object to the end of the todos array
+        setTodos([
+            ...todos, { text: inputText, completed: false, id: parseInt(Math.random() * 1000) },
+        ]);
+
+        // Set state of field back to ""
+        setInputText("");
+
+        console.log("Hey!");
+    };
 
 
     return(
         <form>
             <h3>This is a form</h3>
-            <input onChange={inputTextHandler} type='text' />
+
+            {/* UI of input doesn't change unless it is using a state variable */}
+            <input onChange={inputTextHandler} value={inputText} type='text' />
+
+            <button onClick={submitTodoHandler} type="submit">Submit!</button>
+
             <FontAwesomeIcon icon={faEdit} />
         </form>
     );
